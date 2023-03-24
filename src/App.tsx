97511b72fa83d3145f1c6741f164ae68a3037f0c
zidniryi123 <zidniryi123@gmail.com>
2023-03-24 17:12:07 +0700

    sentry with react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [secondNumber, setSecondNumber] = useState(0)
+  const [resultNumber, setResultNumber] = useState(0)
+
+  const sumNumber = () => {
+    const sumData = firstNumber + secondNumber
+    setResultNumber(sumData)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count Sum App</h1>
+      <form>
+        <label>First Number </label>
+        <input type="number" value={firstNumber} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFirstNumber(parseInt(e.target.value))} />
+        <label >Secon Number</label>
+        <input type="number" value={secondNumber} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSecondNumber(parseInt(e.target.value))} />
+
+        <input type="button" value="Calculate" style={{ backgroundColor: '#1756A1' }} onClick={sumNumber} />
+        {/* Uncoment this if you want to know the errors  from sentry*/}
+        {/* <input type="button" value="Wrong Method" style={{ backgroundColor: 'red' }} onClick={() => methodNotExist()} /> */}
+        <br />
+        <b>Result {resultNumber}</b>
+        <br />
+      </form>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+Sentry.init({
+  // Change with your key api
+  dsn: "https://d6a0f2db354645bdb94c9a8310e65cb1@o4504834911502336.ingest.sentry.io/4504887114596352",
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
 root.render(
   <React.StrictMode>
     <App />
